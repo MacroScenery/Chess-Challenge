55744d3ffd7db90d18495dac85a445a00c249a13
Sean Lynch <sean04elmo@gmail.com>
2023-08-02 12:33:00 -0400

    Added System Namespace & tested writing to console

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,13 @@
 ﻿using ChessChallenge.API;
 
+using System;
+
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
+        //Console.WriteLine("Test");
         return moves[0];
     }
 }
